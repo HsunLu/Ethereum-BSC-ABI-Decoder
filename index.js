dfd52b644699797b3ef3783fff4e708fff7572e3
HsunLu <hugh831016@gmail.com>
2023-04-19 19:31:43 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+function decode() {
+  const abi = JSON.parse(document.getElementById("abi").value);
+  const data = document.getElementById("data").value;
+  const decoder = document.getElementById("decoder").value;
+  let decodedData;
+  if (decoder === "ethers") {
+    decodedData = ethers.utils.defaultAbiCoder.decode(abi, data);
+  } else if (decoder === "abi-decoder") {
+    AbiDecoder.decodeMethod(data, abi);
+    decodedData = AbiDecoder.getMethodReturn();
+  }
+  document.getElementById("decoded").textContent = JSON.stringify(decodedData, null, 2);
+}
