d9ed9dfae61bde90dcd76077c31041a43cde4692
HsunLu <hugh831016@gmail.com>
2023-04-19 22:35:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 function decode() {
   const abi = JSON.parse(document.getElementById("abi").value);
-  alert(abi);
   const data = document.getElementById("data").value;
-  alert(data);
   const decoder = document.getElementById("decoder").value;
-  alert(decoder);
   let decodedData;
   if (decoder === "ethers") {
     decodedData = ethers.utils.defaultAbiCoder.decode(abi, data);
@@ -12,7 +9,6 @@ function decode() {
     abiDecoder.addABI(abi)
     decodedData = abiDecoder.decodeMethod(data);
   }
-  alert("123\n"+decodedData);
   document.getElementById("decoded").textContent = JSON.stringify(decodedData, null, 2);
 }
 
